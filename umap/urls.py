294ef7785376a3690e6045a8d698e686fdcfdd1b
Dietmar Aumann <dietmar.aumann@virdata.com>
2015-10-16 05:35:42 +0200

    adding app url

--- umap/urls.py
+++ umap/urls.py
@@ -18,14 +18,23 @@ urlpatterns = patterns(
         (r'^admin/', include(admin.site.urls)),
         url('', include('social.apps.django_app.urls', namespace='social')),
         url(r'^m/(?P<pk>\d+)/$', MapShortUrl.as_view(), name='umap_short_url'),
-        url(r'^ajax-proxy/$', cache_page(180)(views.ajax_proxy), name='ajax-proxy'),
-        '',
+        url(r'^ajax-proxy/$', cache_page(180)(views.ajax_proxy), name='ajax-proxy')
+        )
+        )
+    )
+)
+
+urlpatterns += i18n_patterns(
+    '',
+    url(r'^app/', include(patterns(
         url(r'^$', views.home, name="home"),
         url(r'^showcase/$', cache_page(24 * 60 * 60)(views.showcase), name='maps_showcase'),
         url(r'^search/$', views.search, name="search"),
         url(r'^about/$', views.about, name="about"),
         url(r'^user/(?P<username>[-_\w@]+)/$', views.user_maps, name='user_maps'),
         (r'', include('leaflet_storage.urls'))
+        )
+        )
     )
 )
 
