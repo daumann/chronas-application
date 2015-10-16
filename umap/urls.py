c8487f68e2a69b53246b11172ba10799bcf0be69
Dietmar Aumann <dietmar.aumann@virdata.com>
2015-10-16 06:11:08 +0200

    rerouting

--- umap/urls.py
+++ umap/urls.py
@@ -14,28 +14,19 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^app/', include(patterns(
-        (r'^admin/', include(admin.site.urls)),
-        url('', include('social.apps.django_app.urls', namespace='social')),
-        url(r'^m/(?P<pk>\d+)/$', MapShortUrl.as_view(), name='umap_short_url'),
-        url(r'^ajax-proxy/$', cache_page(180)(views.ajax_proxy), name='ajax-proxy')
-        )
-        )
-    )
+    (r'^app/admin/', include(admin.site.urls)),
+    url('', include('social.apps.django_app.urls', namespace='social')),
+    url(r'^app/m/(?P<pk>\d+)/$', MapShortUrl.as_view(), name='umap_short_url'),
+    url(r'^app/ajax-proxy/$', cache_page(180)(views.ajax_proxy), name='ajax-proxy'),
 )
-
 urlpatterns += i18n_patterns(
     '',
-    url(r'^app/', include(patterns(
-        url(r'^$', views.home, name="home"),
-        url(r'^showcase/$', cache_page(24 * 60 * 60)(views.showcase), name='maps_showcase'),
-        url(r'^search/$', views.search, name="search"),
-        url(r'^about/$', views.about, name="about"),
-        url(r'^user/(?P<username>[-_\w@]+)/$', views.user_maps, name='user_maps'),
-        (r'', include('leaflet_storage.urls'))
-        )
-        )
-    )
+    url(r'^app/$', views.home, name="home"),
+    url(r'^app/showcase/$', cache_page(24 * 60 * 60)(views.showcase), name='maps_showcase'),
+    url(r'^app/search/$', views.search, name="search"),
+    url(r'^app/about/$', views.about, name="about"),
+    url(r'^app/user/(?P<username>[-_\w@]+)/$', views.user_maps, name='user_maps'),
+    (r'', include('leaflet_storage.urls')),
 )
 
 if settings.DEBUG and settings.MEDIA_ROOT:
